6ce39bdc7f377604c4b8d9d3feb68215883eb4c1
oludare89 <azizoyekan@hotmail.com>
2023-01-07 15:03:14 +0000

    Added horizontal movement boundaries

--- js/player.js
+++ js/player.js
@@ -6,10 +6,19 @@ export class Player {
         this.x = 0;
         this.y = this.game.height - this.height;
         this.image = player;
+        this.speed = 0;
+        this.maxSpeed = 10;
     }
     update(input){
-        if (input.includes('ArrowRight')) this.x++;
-        if (input.includes('ArrowLeft')) this. x--;
+        // horizontal movement
+        this.x += this.speed;
+        if (input.includes('ArrowRight')) this.speed = this.maxSpeed;
+        else if (input.includes('ArrowLeft')) this.speed = -this.maxSpeed;
+        else this.speed = 0;
+        // horizontal boundaries
+        if (this.x < 0) this.x = 0;
+        if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
+        
     }
     draw(context){
         context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
