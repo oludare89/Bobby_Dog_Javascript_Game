607113aff8e75ca3f33529e41d943e469c8be458
oludare89 <azizoyekan@hotmail.com>
2023-01-07 16:12:21 +0000

    Created two states - sitting and running

--- js/player.js
+++ js/player.js
@@ -1,3 +1,5 @@
+import { Sitting, Running } from './playerStates.js';
+
 export class Player {
     constructor(game){
         this.game = game;
@@ -8,10 +10,16 @@ export class Player {
         this.vy = 0;
         this.weight = 1;
         this.image = player; // we can directly use the id for the player image instead of getElementByID
+        this.frameX = 0;
+        this.frameY = 0;
         this.speed = 0;
         this.maxSpeed = 10;
+        this.states = [new Sitting(this), new Running(this)];
+        this.currentState = this.states[0];
+        this.currentState.enter();
     }
     update(input){
+        this.currentState.handleInput(input);
         // horizontal movement
         this.x += this.speed;
         if (input.includes('ArrowRight')) this.speed = this.maxSpeed;
@@ -21,15 +29,19 @@ export class Player {
         if (this.x < 0) this.x = 0;
         if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
         // vertical movement
-        if (input.includes('ArrowUp') && this.onGround()) this.vy -= 10;
+        if (input.includes('ArrowUp') && this.onGround()) this.vy -= 30;
         this.y += this.vy;
         if (!this.onGround()) this.vy += this.weight;
         else this.vy = 0;
     }
     draw(context){
-        context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+        context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
     }
     onGround(){
         return this.y >= this.game.height - this.height;
+    }
+    setState(state){
+        this.currentState = this.states[state];
+        this.currentState.enter();
     }
 }
