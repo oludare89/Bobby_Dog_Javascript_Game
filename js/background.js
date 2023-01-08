eda1490c99411dd22e41e1a6d7c2c0df557ef2c2
oludare89 <azizoyekan@hotmail.com>
2023-01-08 22:01:47 +0000

    Created parallex endless scrolling background

--- js/background.js
+++ js/background.js
@@ -9,9 +9,40 @@ class Layer {
         this.y = 0;
     }
     update(){
-
+        if (this.x < -this.width) this.x = 0;
+        else this.x -= this.game.speed * this.speedModifier;
     }
-    draw(){
-        
+    draw(context){
+        context.drawImage(this.image, this.x, this.y, this.width, this.height);
+        context.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+    }
+}
+
+export class Background {
+    constructor(game){
+        this.game = game;
+        this.width = 1667;
+        this.height = 500;
+        this.layerImage1 = layer1;
+        this.layerImage2 = layer2;
+        this.layerImage3 = layer3;
+        this.layerImage4 = layer4;
+        this.layerImage5 = layer5;
+        this.layer1 = new Layer (this.game, this.width, this.height, 0, this.layerImage1);
+        this.layer2 = new Layer (this.game, this.width, this.height, 0.2, this.layerImage2);
+        this.layer3 = new Layer (this.game, this.width, this.height, 0.4, this.layerImage3);
+        this.layer4 = new Layer (this.game, this.width, this.height, 0.8, this.layerImage4);
+        this.layer5 = new Layer (this.game, this.width, this.height, 1, this.layerImage5);
+        this.backgroundLayers = [this.layer1, this.layer2, this.layer3, this.layer4, this.layer5];
+    }
+    update(){
+        this.backgroundLayers.forEach(layer => {
+            layer.update();
+        })
+    }
+    draw(context){
+        this.backgroundLayers.forEach(layer => {
+            layer.draw(context);
+        })
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { Player } from './player.js';
 import { InputHandler } from './inputs.js';
+import { Background } from './background.js';
 
 window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1');
@@ -12,13 +13,17 @@ window.addEventListener('load', function(){
             this.width = width;
             this.height = height;
             this.groundMargin = 50;
+            this.speed = 3;
+            this.background = new Background(this);
             this.player = new Player(this);
             this.input = new InputHandler();
         }
         update(deltaTime){
+            this.background.update();
             this.player.update(this.input.keys, deltaTime);
         }
         draw(context){
+            this.background.draw(context)
             this.player.draw(context);
         }
     }
