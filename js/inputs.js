31ef476b33ac6f32d0bdc9f34f95045448f5a1b2
oludare89 <azizoyekan@hotmail.com>
2023-01-19 21:11:15 +0000

    Added UI class that draws game score on to game

--- js/inputs.js
+++ js/inputs.js
@@ -10,7 +10,6 @@ export class InputHandler {
                     e.key === 'Enter'
                 ) && this.keys.indexOf(e.key) === -1){
                 this.keys.push(e.key);
-                console.log(this.keys);
             } else if (e.key === 'd') this.game.debug = !this.game.debug;
         });
         window.addEventListener('keyup', e => {
