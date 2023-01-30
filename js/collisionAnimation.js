52bad320fff151c4416327fdf7310ffc9480cb29
oludare89 <azizoyekan@hotmail.com>
2023-01-30 17:18:08 +0000

    Added explosions when enemies are hit by the player

---
+++ js/collisionAnimation.js
@@ -0,0 +1,32 @@
+export class CollisionAnimation {
+    constructor(game, x, y){
+        this.game = game;
+        this.image = boom;
+        this.spriteWidth = 100;
+        this.spriteHeight = 90;
+        this.sizeModifier = Math.random() + 0.5;
+        this.width = this.spriteWidth * this.sizeModifier;
+        this.height = this.spriteHeight * this.sizeModifier;
+        this.x = x - this.width * 0.5;
+        this.y = y - this.height * 0.5;
+        this.frameX = 0;
+        this.maxFrame = 4;
+        this.markedForDeletion = false;
+        this.frameTimer = 0;
+        this.fps = 20;
+        this.frameInterval = 1000 / this.fps;
+    }
+    update(deltaTime){
+        this.x -= this.game.speed;
+        if (this.frameTimer > this.frameInterval){
+            this.frameTimer = 0;
+            if (this.frameX < this.maxFrame) this.frameX++;
+            else this.frameX = 0;
+        } else {
+                this.frameTimer += deltaTime
+            }
+    }
+    draw(context){
+        context.drawImage(this.image, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    }
+}
